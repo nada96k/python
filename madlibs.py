23f07bcc63f5ab0edf95ed3b87a90c47119b128d
Your Namenada96k <nadawe_96@hotmail.com>
2019-09-11 22:07:35 +0300

    madlibs creation is done

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+
+print('')
+print("Mad libs where libs get mad.")
+print("Start below:")
+print('')
+time=input('Enter a number from 1 to 12:')
+items=input('Enter a noun (plural): ')  
+name=input("Enter a name: ")
+scream=input("Enter any sentence: ")
+action=input('Enter a verb: ')
+
+print('')
+
+
+
+print('     It was ' , time , "o'clock when I heard a knock at the door.")
+print('     I opened the door and there was a box full of ', items , 'with a note saying "From Mr.' , name.title() , '."')
+print('     Just as I closed the door I heard a scream "', scream.upper() , '."')
+print('     I froze in place and all I could do was ', action , '.')
+ 
